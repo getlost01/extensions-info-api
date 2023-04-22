05ccdd73b1a25decfb545cbeb62cc0d97790f08e
Aagam Jain <aagamjain573@gmail.com>
2023-04-22 08:03:01 +0530

    update userIDs in schema

--- models/CPCalendar.js
+++ models/CPCalendar.js
@@ -8,7 +8,7 @@ const collectDataSchema = new Schema({
     macUser: { type: Number, required: true , default: 0},
     windowUser: { type: Number, required: true , default: 0},
     linuxUser: { type: Number, required: true , default: 0},
-    userID: { type: [String], required: true , default: []},
+    userIDs: { type: [Schema.Types.Mixed], required: true , default: {}},
     newUser: { type: Number, required: true , default: 0},
 	createdAt: String
 });
